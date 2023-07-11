ad8fbb1bf1bd6dfea80550395fd28e5e963e1a6c
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-07-11 10:26:57 +0500

    initialize the tailwind

--- src/components/SocialCalculator.jsx
+++ src/components/SocialCalculator.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import insta from "../assets/insta.png";
 
 const SocialCalculator = () => {
+  const instaHandle = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
   return (
-    <div>SocialCalculator</div>
-  )
+    <div>
+      <div className="flex flex-col align-center py-12 gap-4">
+        <div className="text-3xl text-center font-bold grad-text">
+          Social Engagement Calculator
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SocialCalculator
